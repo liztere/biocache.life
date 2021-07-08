a0ffa9cc516837e46c340f20a94294ac36f2475c
nu-soccer <8105@protonmail.com>
2021-07-07 23:37:58 -0400

    l

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ $(document).ready(function () {
     }
   });
 });
+
+// particle.min.js hosted on GitHub (https://github.com/JulianLaval/canvas-particle-network)
+
+var canvasDiv = document.getElementById('particle-canvas');
+var options = {
+  particleColor: '#000000',
+	background: '#ffffff',
+ 
+  interactive: true,
+  speed: 'slow',
+  density: 'medium'
+};
+var particleCanvas = new ParticleNetwork(canvasDiv, options);
